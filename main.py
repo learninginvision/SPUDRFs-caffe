3a9a651ac8c03d7c40640d6f7ad83b877213ba8c
Learning in Vision <65346642+learninginvision@users.noreply.github.com>
2020-09-02 08:48:56 +0800

    train SPUDRFs from scratch

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ for pace in range(0, len(alpha)+1):
     train_dict['pace'] = pace
     train_dict['record'] = str(pace) + 'VGG.record'
     train_dict['data'] = str(pace) + 'VGG'
+
     train_dict['save'] = './checkpoints/M' + str(Exp) 
     train_dict['tmp_dir'] = './tmp/Exp' + str(Exp) 
-    train_dict['testtxt'] = './images/MORPH-test.txt'
 
     if pace == 0:
         train_dict['traintxt'] = left_txt
@@ -37,10 +37,11 @@ for pace in range(0, len(alpha)+1):
 
         train_dict['base_weights'] = os.path.join('./checkpoints/M' + str(Exp), str(pace-1) + 'VGG_iter_{}.caffemodel'.format(max_step)) # Changed by xgtu
 
+    train_dict['testtxt'] = './images/MORPH-test.txt'
+
     print(train_dict)
     net = train.SPUDRFs(parser_dict=train_dict)
     net.train()
-    
     with open('./Entropy.txt', 'r') as f:
         lines = f.readlines()
     assert len(lines) > 2, 'train entropy.txt is null!'
@@ -54,15 +55,17 @@ for pace in range(0, len(alpha)+1):
         left_lines = f.readlines()
     if len(left_lines) > 0:
         pred_dict = {}
-        pred_dict['test'] = os.path.join('./images/txt-'+ str(Exp), 'trainLeft' + str(pace) + '.txt') 
+
+        pred_dict['test'] = os.path.join('./images/txt'+ str(Exp), 'trainLeft' + str(pace) + '.txt') 
         pred_dict['predict'] = os.path.join('./MAE/mae' + str(Exp), 'MAEOnTrainLeft' + str(pace) + '.txt') 
         pred_dict['deploy'] = os.path.join(train_dict['tmp_dir'], str(pace) + 'VGG-deploy.prototxt')
-        pred_dict['model'] = os.path.join(train_dict['save'], str(pace) + 'VGG_iter_{}.caffemodel'.format(max_step)) 
 
+        pred_dict['model'] = os.path.join(train_dict['save'], str(pace) + 'VGG_iter_{}.caffemodel'.format(max_step)) 
         diff_ave = predict.Predict(pred_dict)
         with open('./Entropy.txt', 'r') as f:
             lines = f.readlines()
         fn_save_entropy = os.path.join('./entropy/E' + str(Exp), 'entropy' + str(pace) + '.txt') 
+        with open(fn_save_entropy, 'w') as f:
             f.writelines(lines)
 
         assert len(lines) > 2, 'predict entropy.txt is null!'
